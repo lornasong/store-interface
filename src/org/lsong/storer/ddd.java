3ae7a098a1625edd62f423c4671c39fa14523360
lornasong <lsong@achievementnetwork.org>
2014-11-15 17:51:29 -0500

    Initial upload frame
    - uploading only test classes

---
+++ src/org/lsong/storer/ddd.java
@@ -0,0 +1,5 @@
+package org.lsong.storer;
+
+public class ddd {
+
+}
---
+++ test/org/lsong/storer/dddTest.java
@@ -0,0 +1,14 @@
+package org.lsong.storer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class dddTest {
+
+	@Test //write this before each test. method should be name test___()
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
